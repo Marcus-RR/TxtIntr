06f691d9605a3c8def919d41e7ccc0a3a2171c8e
Marcus-RR <zybziba@yandex.ru>
2023-10-09 02:10:50 +0300

    calcul

--- calculator.cpp
+++ calculator.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
- 
-bool is_valid(char action)
-{
-    return
-        action == '*' ||
-        action == '/';
-}
- 
-int main(int argc, char **argv)
-{
-    if(argc == 1) { 
-        std::cout << "Мультипликативный, который вычисляет * и / " << std::endl;
-        return 0;
-        }
-    double x;
-    std::cin >> x;
- 
-    char action;
-    std::cin >> action;
- 
-    while (!is_valid(action))
-    {
-        std::cin >> action;
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 4 || argc > 6) {
+        cout << "Мультипликативный, который делает умножение и деление" << endl;
+        cout << "Как использовать: ./calculator <m or d> <число 1> <число 2>,\n так же можно но не обязательно -> [<число 3>] [<число 4>]" << endl;
+        return 1;
     }
- 
-    double y;
-    std::cin >> y;
- 
-    double result;
-    if (action == '*')
-        result = x * y;
-    else
-        result = x / y;
- 
-    std::cout << " " << result;
+
+    string operation = argv[1];
+    double result = 1.0;
+
+    if (operation == "m") {
+        for (int i = 2; i < argc; i++) {
+            double operand = stod(argv[i]);
+            result *= operand;
+        }
+    }
+    else if (operation == "d") {
+        if (argc == 4) {
+            double operand1 = stod(argv[2]);
+            double operand2 = stod(argv[3]);
+            result = operand1 / operand2;
+        }
+        else if (argc == 5) {
+            double operand1 = stod(argv[2]);
+            double operand2 = stod(argv[3]);
+            double operand3 = stod(argv[4]);
+            result = (operand1 / operand2) / operand3;
+        }
+        else if (argc == 6) {
+            double operand1 = stod(argv[2]);
+            double operand2 = stod(argv[3]);
+            double operand3 = stod(argv[4]);
+            double operand4 = stod(argv[5]);
+            result = ((operand1 / operand2) / operand3) / operand4;
+        }
+    }
+    else {
+        cout << "Невозможно" << endl;
+        return 1;
+    }
+
+    cout << result << endl;
+
+    return 0;
 }
